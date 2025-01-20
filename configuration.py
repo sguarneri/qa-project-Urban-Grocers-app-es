7cc69b5e77beddff5b3579f8a59965f62c3d54f2
Sebastian G. Rivera <sguarneros91@icloud.com>
2025-01-19 19:51:27 -0600

    Se actualizó la URL_SERVICE para nueva revisión

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-eb646ebb-190d-4d33-9376-dc9af0e30121.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-bfc42ab5-c33d-41fa-bd36-edb23ca80d38.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users" #Ruta para crear un usuario.
 KITS_PATH = "/api/v1/kits" #Ruta para crear un kit.
